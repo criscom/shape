707160458780bb902186ba8db779be36e0aeaa7b
Christoph Potzinger <criscom27@gmail.com>
2014-05-27 13:56:14 +0200

    Frontpage slider in default.html integriert. default-master.html für typo3 verwenden.

--- templates/layouts/index.php
+++ templates/layouts/index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-    <title>Theme Template for Bootstrap</title>
+    <title>Anto Paar ShapeTec GmbH</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
@@ -17,7 +17,7 @@
 
     <!-- Custom styles for this template -->
     <link href="../css/navbar-static-top.css" rel="stylesheet">
-    <link href="../css/custom.css" rel="stylesheet">
+    <link href="../css/styles.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -39,8 +39,8 @@
           <div class="header-search header-right">
             <form class="searchform" action="" method="get">
               <div class="search-visible">
-                <label class="search-text">Suche</label>
-                <button type="button" class="btn btn-mini" data-toggle="collapse" data-target="#search">Open</button>
+                <!-- <label class="search-text">Suche</label> -->
+                <button type="button" class="search-button" data-toggle="collapse" data-target="#search">Suche</button>
               </div>
               <div class="search-toggle collapse" id="search">
               <input>
@@ -95,20 +95,79 @@
       </div>
     </div>
 
-    <div class="container theme-showcase" role="main">
+    <div class="container theme-showcase content-container" role="main">
+      <!-- Breadcrumbs -->
+      <div class="breadcrumb">
+        <span><a href="#" title="Startseite" rel="v:url" property="v:title">Startseite</a></span>
+        <span class="active"><a href="#" title="Produkte" rel="v:url" property="v:title">Produkte</a></span>
+      </div> <!-- /breadcrumbs -->
       <!-- Partials section -->
-asdfasdf
-      <f:if condition="{layout} == 1">
-      <f:render partial="TwoTimesOne.html" arguments="{contentImage:contentImage, contentBelowImage:contentBelowImage, contentMain:contentMain}" />
-      </f:if>
-
-      <f:if condition="{layout} == 2">
-      <f:render partial="TwoTimesOne.html" arguments="{contentImage:contentImage, contentBelowImage:contentBelowImage, contentMain:contentMain}" />
-      </f:if>
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>One more for good measure.</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div><!-- /.carousel -->
 
       <!-- Partials section END --> 
 
     </div> <!-- /container -->
+
+    <div id="footer">
+      <div class="footer-image">
+        <div class="container"></div>
+      </div>
+      
+      <div class="footer-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-7 col-lg-8">Anton Paar ShapeTec GmbH</div>
+            <div class="col-xs-12 col-sm-6 col-md-5 col-lg-4">
+              <ul class="nav navbar-nav">
+                <li><a href="#">Sitemap</a></li>
+                <li><a href="#">AGB/Impressum</a></li>
+                <li><a href="#">Nutzungsbedingungen</a></li>
+            </div>
+        </div>
+    </div> <!-- /footer -->
 
 
     <!-- Bootstrap core JavaScript
